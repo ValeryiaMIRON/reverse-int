34cfc35a8c1eaf8a1c9eb43ac992e51ab68e409f
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-03-12 13:30:01 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0){
+        let str = n + "";
+      return Number(str.split('').reverse().join(''));
+      } else {
+          if (n < 0) {
+        let str = -(n) + "";
+        return Number(str.split('').reverse().join(''));
+          }
+        }
 }
